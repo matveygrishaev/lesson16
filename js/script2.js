0b96c4bb0696c39717f7e5104b8dea91207ed4c7
matveygrishaev <matveygrishaev@gmail.com>
2020-11-01 23:52:52 +0300

    lesson16 part 2 start homework

---
+++ js/script2.js
@@ -0,0 +1,20 @@
+'use strick';
+
+const myLesson = [
+  {lesson: 1, type: 'basic', points: 2},
+  {lesson: 2, type: 'additional', points: 4},
+  {lesson: 3, type: 'basic', points: 6},
+  {lesson: 4, type: 'additional', points: 3},
+  {lesson: 5, type: 'basic', points: 4},
+  {lesson: 6, type: 'basic', points: 2},
+  {lesson: 7, type: 'additional', points: 2},
+  {lesson: 8, type: 'basic', points: 6},
+  {lesson: 9, type: 'basic', points: 4},
+  {lesson: 10, type: 'basic', points: 6},
+  {lesson: 11, type: 'additional', points: 5}, 
+  {lesson: 12, type: 'basic', points: 2}, 
+  {lesson: 13, type: 'additional', points: 2}, 
+  {lesson: 14, type: 'basic', points: 4},
+  {lesson: 15, type: 'additional', points: 1},
+  {lesson: 16, type: 'additional', points: 7},
+];
